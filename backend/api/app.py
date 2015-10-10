f7184dc085c68bcac2c682138e585083650a1ed3
jasongwartz <jason@jasongwartz.com>
2015-10-10 15:22:20 +0100

    added first app.py api response

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,10 +1,18 @@
 from flask import Flask, jsonify
+import _mysql as mdp
+import read_db as db
+
 
 app = Flask(__name__)
 
+   
+
+data_library = db.main()
+
+
 @app.route('/building/<int:building_id>')
 def index(building_id):
-    return "Hello, World! " + str(building_id)
+    return jsonify({building_id:data_library[building_id]})
 
 
 if __name__ == '__main__':
