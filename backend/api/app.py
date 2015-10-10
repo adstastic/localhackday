18c025a0adf6a5c4c43d48105813a372a98a72e8
jasongwartz <jason@jasongwartz.com>
2015-10-10 17:51:04 +0100

    api work - post request

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,19 +1,61 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+from flask.ext.httpauth import HTTPBasicAuth
+
 import _mysql as mdp
 import read_db as db
-
+import json
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 
-  
+@auth.get_password
+def get_password(username):
+    if username == 'admin':
+        with open("config.json", "rb") as fp:
+            config = json.loads(fp.read())
+        return config["api_pass"]
+    return None
 
-@app.route('/room/<int:building_id>')
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
+@app.route('/api/room/<int:room_id>')
 def index(room_id):
-    return jsonify(by_id(room_id))
+    return jsonify(db.by_id(room_id))
 
-@app.route('/search/<query>')
+@app.route('/api/search/<query>')
 def search(query):
     return jsonify(db.search_name(query))
 
+@app.route('/api/room', methods=['POST'])
+@auth.login_required
+def create_room():
+  #  if not request.json: # or not 'name' in request.json:
+  #      abort(400)
+    print 'made it before vars'
+    if request.data:
+        print "HAS DATA"
+    else:
+        print "NO DATA"
+    name = request.json['name']
+    size = request.json['size']
+    room_type = request.json['type']
+    htmlref = request.json['htmlref']
+    print 'made it before new'
+    new = db.add_new(name, size, room_type, htmlref)
+    print 'made it after new'
+    return jsonify(new), 201
+    
+    
+    
+    
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': '404 Not found'}), 404)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- backend/api/read_db.py
+++ backend/api/read_db.py
@@ -14,14 +14,26 @@ def search_name(query):
 		data[row[0]] = {"name":row[1], "size":row[2], "type":row[3]}
 
 def by_id(id_num):
-	
 	cur = connect().cursor()
-	cur.execute('SELECT * FROM rooms WHERE id=%s' %id_num)
-	
+	cur.execute('SELECT * FROM rooms WHERE id={}'.format(id_num))
 	response = cur.fetchall()
-	return [row for row in response]
+	return {row['id']:[row['name'], row['size'], row['type'], row['htmlref']] for row in response}
 	
+def add_new(name, size, room_type, htmlref):
 	
+	con = connect()
+	cur = con.cursor()
+	string = 'INSERT INTO rooms (id, name, size, type, htmlref) VALUES (NULL, "{}", {}, "{}", "{}");'.format(name, size, room_type, htmlref)
+	print string
+	cur.execute(string)
+	cur.execute('SELECT * FROM rooms WHERE name="{}"'.format(name))
+	con.commit()
+	confirm = cur.fetchall()
+	confirm_data = {confirm[0][0]:[i for i in confirm[0]]}
+	return confirm_data
+
+
+
 def connect():
 	
 	with open("config.json", "rb") as fp:
